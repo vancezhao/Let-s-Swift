acd328a156d1f353669af9397abf125d921769bb
Vance <vancezhao@gmail.com>
2015-02-26 16:16:53 +0800

    Initial Commit

---
+++ Let us Swift v2Tests/Let_us_Swift_v2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Let_us_Swift_v2Tests.swift
+//  Let us Swift v2Tests
+//
+//  Created by vance zhao on 15/2/26.
+//  Copyright (c) 2015年 vance zhao. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Let_us_Swift_v2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
